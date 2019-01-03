1dd11129f27c13a1d4eece164a806a6e71788b1d
Jim <358369859@qq.com>
2019-01-04 00:01:29 +0800

    将标题栏剥离为通用自定义组件

---
+++ lib/custom_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// 无状态变化的组件，所有成员变量必须是final的
+class MyAppBar extends StatelessWidget {
+  final Widget wTitle;
+  final String title;
+
+  MyAppBar({this.wTitle, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    if (wTitle != null) {
+      return getTitleContainer(wTitle);
+    } else {
+      return getTitleContainer(
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation Menu',
+              onPressed: null,
+            ),
+            Expanded(
+              child: Text(
+                title,
+                textDirection: TextDirection.ltr,
+                style: Theme.of(context).primaryTextTheme.title,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            )
+          ],
+        ),
+      );
+    }
+  }
+}
+
+Container getTitleContainer(Widget child) {
+  return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+      // 中括号里只能填写小于1000的100的整数，数值越大颜色越深，具体意思未知
+      decoration: BoxDecoration(color: Colors.blue[200]),
+      child: child);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'custom_widget.dart';
 
 void main() {
   runApp(MaterialApp(title: "Hello Flutter", home: MyScaffold()
@@ -17,51 +18,12 @@ class MyScaffold extends StatelessWidget {
     return Material(
       child: Column(
         children: <Widget>[
-          MyAppBar(
-            wTitle: Text(
-              'Practice',
-              textDirection: TextDirection.ltr,
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
+          MyAppBar(title: 'Practice Lesson 2',),
           Expanded(
             child: Text(
               'This is my truely Hello World!',
               textDirection: TextDirection.ltr,
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  final Widget wTitle;
-
-  MyAppBar({this.wTitle});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-      decoration: BoxDecoration(color: Colors.blue[200]),
-      // 中括号里只能填写小于1000的100的整数，数值越大颜色越深，具体意思未知
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation Menu',
-            onPressed: null,
-          ),
-          Expanded(
-            child: wTitle,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
           )
         ],
       ),
