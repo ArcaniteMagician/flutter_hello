9ad718f703e2becb9bc4d4ff7386b73b8937964a
Jim <358369859@qq.com>
2019-01-05 00:02:41 +0800

    Material Components基础练习，大概理解到了Scaffold是什么意思，感觉像是一套根据参数给出固定样式的UI模板，使用起来会很方便快捷

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'custom_widget.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Hello Flutter", home: MyScaffold()
+  runApp(MaterialApp(title: "Hello Flutter", home: TutorialHome()
 //    Center(
 //      child: Text(
 //        'This is my truely Hello World!',
@@ -12,13 +12,50 @@ void main() {
       ));
 }
 
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null),
+        title: Text('Tutorial'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add
+            // 这个child难道默认就是个IconButton？
+//        IconButton(
+//          icon: Icon(Icons.add),
+//          onPressed: null,
+//          tooltip: 'Add',
+            ),
+      ),
+    );
+  }
+}
+
 class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Column(
         children: <Widget>[
-          MyAppBar(title: 'Practice Lesson 2',),
+          MyAppBar(
+            title: 'Practice Lesson 2',
+          ),
           Expanded(
             child: Text(
               'This is my truely Hello World!',
