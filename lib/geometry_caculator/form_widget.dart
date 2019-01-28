cead65b3763d4db65a34f6ac8d999c5e9e250c81
Jim <358369859@qq.com>
2019-01-28 22:46:29 +0800

    木匠几何，30°内角直角三角形，提取公共组件

---
+++ lib/geometry_caculator/form_widget.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class TitleRow extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String data;
+
+  const TitleRow({Key key, this.title, this.subtitle, this.data})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: _TipText(title),
+        ),
+        Expanded(
+          flex: 2,
+          child: _TipText(subtitle),
+        ),
+        Expanded(
+          flex: 3,
+          child: _TipText(data),
+        ),
+      ],
+    );
+  }
+}
+
+class MyDataRow extends StatelessWidget {
+  final String name;
+  final String angle;
+  final Widget valueText;
+
+  const MyDataRow({this.name, this.angle, this.valueText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: _TipText(name),
+        ),
+        Expanded(
+          flex: 2,
+          child: _TipText(angle),
+        ),
+        Expanded(
+          flex: 3,
+          child: valueText,
+        )
+      ],
+    );
+  }
+}
+
+class LengthTextField extends StatefulWidget {
+  final String editingValue;
+  final String decorationText;
+  final CalculateCallback callback;
+
+  LengthTextField(
+      {Key key, this.decorationText, this.editingValue, this.callback})
+      : super(key: key);
+
+  @override
+  LengthTextFieldState createState() => LengthTextFieldState();
+}
+
+class LengthTextFieldState extends State<LengthTextField> {
+  String _decorationText;
+  String _editingValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _editingValue = widget.editingValue;
+    _decorationText = widget.decorationText;
+  }
+
+  void setNewValue(String decorationText, String newValue) {
+    setState(() {
+      // TODO- 过长内容取到小数点后四位为止
+//      if (newValue.length > 10) {
+//        int endIndex = newValue.indexOf(".", newValue.length) + 4;
+//        if (endIndex > newValue.length) {
+//          endIndex = newValue.length;
+//        }
+//        newValue = newValue.substring(0, endIndex);
+//      }
+      this._decorationText = decorationText;
+      this._editingValue = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // TextField样式参考：https://juejin.im/post/5b6bdb406fb9a04f89785cb5
+      child: TextField(
+        keyboardType: TextInputType.number,
+        onChanged: (value) {
+          double num = double.tryParse(value) ?? -1;
+          if (num > 0) {
+            widget.callback(value, num);
+          }
+        },
+        decoration: InputDecoration(
+          labelText: _decorationText,
+        ),
+        controller: TextEditingController.fromValue(TextEditingValue(
+            text: _editingValue,
+            // 保持光标在文末
+            selection: TextSelection.fromPosition(TextPosition(
+                affinity: TextAffinity.downstream,
+                offset: _editingValue.length)))),
+      ),
+    );
+  }
+}
+
+class _TipText extends StatelessWidget {
+  final String text;
+
+  const _TipText(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+typedef CalculateCallback<String> = void Function(String text, double value);
--- lib/geometry_caculator/isosceles_right_triangle.dart
+++ lib/geometry_caculator/isosceles_right_triangle.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hello/geometry_caculator/form_widget.dart';
 
-final shortKey = GlobalKey<_LengthTextFieldState>();
-final middleKey = GlobalKey<_LengthTextFieldState>();
-final longKey = GlobalKey<_LengthTextFieldState>();
+final shortKey = GlobalKey<LengthTextFieldState>();
+final middleKey = GlobalKey<LengthTextFieldState>();
+final longKey = GlobalKey<LengthTextFieldState>();
 
 class IsoscelesRightTriangle extends StatefulWidget {
   @override
@@ -20,8 +21,12 @@ class _IsoscelesRightTriangleState extends State<IsoscelesRightTriangle> {
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
         child: ListView(
           children: <Widget>[
-            _TitleRow(),
-            _DataRow(
+            TitleRow(
+              title: '代号',
+              subtitle: '对应内角\n度数',
+              data: '边长',
+            ),
+            MyDataRow(
               name: 'a',
               angle: '30°',
               valueText: LengthTextField(
@@ -37,7 +42,7 @@ class _IsoscelesRightTriangleState extends State<IsoscelesRightTriangle> {
                 },
               ),
             ),
-            _DataRow(
+            MyDataRow(
               name: 'b',
               angle: '60°',
               valueText: LengthTextField(
@@ -53,7 +58,7 @@ class _IsoscelesRightTriangleState extends State<IsoscelesRightTriangle> {
                 },
               ),
             ),
-            _DataRow(
+            MyDataRow(
               name: 'c',
               angle: '90°',
               valueText: LengthTextField(
@@ -75,138 +80,3 @@ class _IsoscelesRightTriangleState extends State<IsoscelesRightTriangle> {
     );
   }
 }
-
-class _TitleRow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: _TipText('代号'),
-        ),
-        Expanded(
-          flex: 2,
-          child: _TipText('对应内角\n度数'),
-        ),
-        Expanded(
-          flex: 3,
-          child: _TipText('边长'),
-        ),
-      ],
-    );
-  }
-}
-
-class _DataRow extends StatelessWidget {
-  final String name;
-  final String angle;
-  final Widget valueText;
-
-  const _DataRow({this.name, this.angle, this.valueText});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: _TipText(name),
-        ),
-        Expanded(
-          flex: 2,
-          child: _TipText(angle),
-        ),
-        Expanded(
-          flex: 3,
-          child: valueText,
-        )
-      ],
-    );
-  }
-}
-
-class LengthTextField extends StatefulWidget {
-  final String editingValue;
-  final String decorationText;
-  final CalculateCallback callback;
-
-  LengthTextField(
-      {Key key, this.decorationText, this.editingValue, this.callback})
-      : super(key: key);
-
-  @override
-  _LengthTextFieldState createState() => _LengthTextFieldState();
-}
-
-class _LengthTextFieldState extends State<LengthTextField> {
-  String _decorationText;
-  String _editingValue;
-
-  @override
-  void initState() {
-    super.initState();
-    _editingValue = widget.editingValue;
-    _decorationText = widget.decorationText;
-  }
-
-  void setNewValue(String decorationText, String newValue) {
-    setState(() {
-      // TODO- 过长内容取到小数点后四位为止
-//      if (newValue.length > 10) {
-//        int endIndex = newValue.indexOf(".", newValue.length) + 4;
-//        if (endIndex > newValue.length) {
-//          endIndex = newValue.length;
-//        }
-//        newValue = newValue.substring(0, endIndex);
-//      }
-      this._decorationText = decorationText;
-      this._editingValue = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      // TextField样式参考：https://juejin.im/post/5b6bdb406fb9a04f89785cb5
-      child: TextField(
-        keyboardType: TextInputType.number,
-        onChanged: (value) {
-          double num = double.tryParse(value) ?? -1;
-          if (num > 0) {
-            widget.callback(value, num);
-          }
-        },
-        decoration: InputDecoration(
-          labelText: _decorationText,
-        ),
-        controller: TextEditingController.fromValue(TextEditingValue(
-            text: _editingValue,
-            // 保持光标在文末
-            selection: TextSelection.fromPosition(TextPosition(
-                affinity: TextAffinity.downstream,
-                offset: _editingValue.length)))),
-      ),
-    );
-  }
-}
-
-class _TipText extends StatelessWidget {
-  final String text;
-
-  const _TipText(this.text);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        text,
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-}
-
-typedef CalculateCallback<String> = void Function(String text, double value);
